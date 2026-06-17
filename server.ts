a8caa3dfc70bf033a4c9de25eaeb711fc1b9e916
Vishal Choudhary <dev.vishal.choudhary@gmail.com>
2026-06-17 09:24:00 +0530

    logs

--- server.ts
+++ server.ts
@@ -8,6 +8,8 @@ import { logger } from "./src/utils/logger";
   await connectRedis();
 })();
 app.use("/api", apiLimiter);
+console.log("PORT:", process.env.PORT);
+console.log("config.port:", config.port);
 app.listen(config.port, () => {
   logger.info(`Server running on port ${config.port}`);
 });
